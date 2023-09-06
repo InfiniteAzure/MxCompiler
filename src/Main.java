4ef2999459e622243f4b0172fc70a80510f42b4e
InfiniteAzure <119796695+InfiniteAzure@users.noreply.github.com>
2023-09-06 19:50:59 +0800

    Codegen debugging

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import Parser.MxLexer;
 import Parser.MxParser;
+import asm.asmModule;
+import asm.asmPrinter;
+import backend.InstructionSelector;
+import backend.RegAllocator;
+import backend.StackAllocator;
 import errors.MxParserErrorListener;
+import ir.IRBuilder;
+import ir.IRPrinter;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import semantic.Semantic;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-//      CharStream input = CharStreams(System.in);
+//        CharStream input = CharStreams(System.in);
         var input = CharStreams.fromStream(System.in);
         var lexer = new MxLexer(input);
         lexer.removeErrorListeners();
@@ -17,5 +27,23 @@ public class Main {
         parser.removeErrorListeners();
         parser.addErrorListener(new MxParserErrorListener());
         var semantic = new Semantic(parser.program());
+
+        var irBuilder = new IRBuilder(semantic.programScope);
+        irBuilder.visit(semantic.root);
+        var llFile = new FileOutputStream("out.ll");
+        var ll = new PrintStream(llFile);
+        var irPrinter = new IRPrinter(ll, "input.mx");
+        irPrinter.print(irBuilder.module);
+        llFile.close();
+
+        var ASMModule = new asmModule();
+        new InstructionSelector(ASMModule).visit(irBuilder.module);
+        new RegAllocator().runOnModule(ASMModule);
+        new StackAllocator().runOnModule(ASMModule);
+
+        var asmFile = new FileOutputStream("output.s");
+        var asm = new PrintStream(asmFile);
+        new asmPrinter(asm).runOnModule(ASMModule);
+        asmFile.close();
     }
 }
