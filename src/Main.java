ec14883239a0180db56f7c68931f1be6dc9338d9
InfiniteAzure <119796695+InfiniteAzure@users.noreply.github.com>
2023-10-15 07:27:31 +0800

    Opt

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
 import Parser.MxLexer;
 import Parser.MxParser;
-import asm.asmModule;
-import asm.asmPrinter;
-import backend.InstructionSelector;
-import backend.RegAllocator;
-import backend.StackAllocator;
+import backend.Backend;
 import errors.MxParserErrorListener;
-import ir.IRBuilder;
-import ir.IRPrinter;
+import optimize.MiddleEnd;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import semantic.Semantic;
 
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-//        CharStream input = CharStreams(System.in);
         var input = CharStreams.fromStream(System.in);
         var lexer = new MxLexer(input);
         lexer.removeErrorListeners();
@@ -25,25 +16,11 @@ public class Main {
         var tokens = new CommonTokenStream(lexer);
         var parser = new MxParser(tokens);
         parser.removeErrorListeners();
+
         parser.addErrorListener(new MxParserErrorListener());
-        var semantic = new Semantic(parser.program());
 
-        var irBuilder = new IRBuilder(semantic.programScope);
-        irBuilder.visit(semantic.root);
-        var llFile = new FileOutputStream("out.ll");
-        var ll = new PrintStream(llFile);
-        var irPrinter = new IRPrinter(ll, "input.mx");
-        irPrinter.print(irBuilder.module);
-        llFile.close();
-
-        var ASMModule = new asmModule();
-        new InstructionSelector(ASMModule).visit(irBuilder.module);
-        new RegAllocator().runOnModule(ASMModule);
-        new StackAllocator().runOnModule(ASMModule);
-
-        var asmFile = new FileOutputStream("output.s");
-        var asm = new PrintStream(System.out);
-        new asmPrinter(asm).runOnModule(ASMModule);
-        asmFile.close();
+        var frontEnd = new Semantic(parser.program());
+        var middleEnd = new MiddleEnd(frontEnd);
+        var backEnd = new Backend(middleEnd);
     }
 }
